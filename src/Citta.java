06c005edc0802356de5bb37a6faf91e4ca5a9035
DylanHP <dylanhp2001@gmail.com>
2021-05-24 17:59:03 +0200

    Primo commit

---
+++ src/Citta.java
@@ -0,0 +1,16 @@
+public class Citta {
+
+    private int id;
+    private String nome;
+    private int x;
+    private int y;
+    private int h;
+
+    public Citta(int id, String nome, int x, int y, int h) {
+        this.id = id;
+        this.nome = nome;
+        this.x = x;
+        this.y = y;
+        this.h = h;
+    }
+}
